c3278482dcc703c73f961b73dc6de479e64e09e8
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2025-09-19 23:08:10 +0700

    fix: try to fix vercel hosting

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require __DIR__ . "/../public/index.php";
+
+// Pastikan path ini benar!
+require __DIR__.'/../vendor/autoload.php';
+
+// Dan pastikan path ini juga benar!
+$app = require_once __DIR__.'/../bootstrap/app.php';
